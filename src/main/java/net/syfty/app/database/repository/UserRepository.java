dd1425d00a683093b051696da195ba86bca25bcc
Erik Anderson <erik.kristofer.anderson@gmail.com>
2023-02-17 19:43:29 -0600

    delete old things

--- src/main/java/net/syfty/app/database/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package net.syfty.app.database.repository;
-
-import net.syfty.app.database.entity.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, Integer> {
-    public User findByEmail(String email);
-
-}
